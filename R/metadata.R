a21d2380912119d35a71b241079c3daab403a503
Martin Smith <martins@gmail.com>
2018-07-18 16:01:47 +0100

    document()

--- R/metadata.R
+++ R/metadata.R
@@ -42,7 +42,7 @@ MorphoBankExports <- function (path='.', ...) {
 #'
 #' Reports which of a list of nexus files was exported from MorphoBank most recently
 #'
-#' @param filenams Character vector listing locations of nexus files exported from MorphoBank
+#' @param filenames Character vector listing locations of nexus files exported from MorphoBank
 #'
 #' @return character of length one, specifying the most recent of the files provided
 #' @author Martin R. Smith
