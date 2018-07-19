5c8324d9b9203191e4ead0de13b08b6d1935e9e7
Martin Smith <martins@gmail.com>
2018-07-19 08:45:50 +0100

    Useful GitLink tests

--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -1,12 +1,8 @@
 context("GitHub link functions")
 test_that("GitHub links work", {
   LinkWorks <- function(url) attr(curlGetHeaders(url), 'status') == 200
-  options('GitHubUser'='ms609')
+  options('GitHubUser'='ms609',GitHubRepo='MorphoBank')
   expect_true(LinkWorks(substr(GitHubPages(repo='hyoliths'), 3, 27)))
-  expect_true(LinkWorks(substr(GitLink(repo='hyoliths'), 3, 27)))
-  expect_true(LinkWorks(
-    substr(GitLink('R/github.R', repo='MorphoBank'), 65, 132)
-    ))
-
-
+  expect_true(LinkWorks(substr(GitLink('R/github.R'), 65, 132)))
+  expect_true(LinkWorks(substr(GitLink('R/github.R', raw=FALSE), 55, 112)))
 })
